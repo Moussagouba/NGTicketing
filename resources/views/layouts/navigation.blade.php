9969e3af2dee5a6e84e93b168dea4ac4681adaa4
goubamoussa <133861194+goubamoussa@users.noreply.github.com>
2024-06-01 15:34:33 +0000

    ajout de la redirection dynamique

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,24 @@
+@php
+$roletype = Auth::user()->role;
+$redirectUrl ='dashboard';
+
+switch($roletype)
+{
+#Super Admin
+case 1:
+$redirectUrl = 'super-admin.dashboard';
+break;
+#Admin
+case 2:
+$redirectUrl = 'admin.dashboard';
+break;
+#utilisateur
+default:
+$redirectUrl = 'dashboard';
+break;
+}
+@endphp
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -5,14 +26,14 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route($redirectUrl) }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route($redirectUrl)" :active="request()->routeIs($redirectUrl)">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
@@ -42,8 +63,7 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -67,7 +87,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route($redirectUrl)" :active="request()->routeIs($redirectUrl)">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -88,8 +108,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
